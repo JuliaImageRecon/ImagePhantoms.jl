3757ce4dde68ef381a6841f8d7a22c63309b1893
Jeff Fessler <fessler@umich.edu>
2022-07-02 15:49:41 -0400

    lit!

--- docs/lit/examples/31-ellipsoid.jl
+++ docs/lit/examples/31-ellipsoid.jl
@@ -29,9 +29,11 @@ using ImagePhantoms: Ellipsoid, phantom, radon, spectrum
 using ImageGeoms: ImageGeom, axesf
 using MIRTjim: jim, prompt, mid3
 using FFTW: fft, fftshift
+using LazyGrids: ndgrid
 using Unitful: mm, unit, °
 using UnitfulRecipes
 using Plots: plot, plot!, scatter!, gui, default
+using Plots # gif @animate
 default(markerstrokecolor=:auto)
 
 
@@ -53,13 +55,12 @@ Here are 3 ways to define an ellipsoid object,
 using parameters specified with physical units.
 =#
 
-if false # todo
-
 center = (20mm, 10mm, 5mm)
-radii = (35mm, 25mm, 15mm)
-angles = (π/6, 0)
-ob = Ellipsoid([40mm, 20mm, 10mm, 35mm, 25mm, 15mm, π/6, 0, 1.0f0])
-ob = Ellipsoid(40mm, 20mm, 10mm, 35mm, 25mm, 15mm, π/6, 0, 1.0f0)
+radii = (25mm, 35mm, 15mm)
+ϕ0s = :(π/6) # symbol version for nice plot titles
+angles = (eval(ϕ0s), 0)
+ob = Ellipsoid([40mm, 20mm, 10mm, 25mm, 35mm, 15mm, π/6, 0, 1.0f0])
+ob = Ellipsoid(40mm, 20mm, 10mm, 25mm, 35mm, 15mm, π/6, 0, 1.0f0)
 ob = Ellipsoid(center, radii, angles, 1.0f0) # recommended use
 
 
@@ -70,21 +71,23 @@ Make a 3D digital image of it using `phantom` and display it.
 We use `ImageGeoms` to simplify the indexing.
 =#
 
-deltas = (1.2mm, 1.1mm, 1mm)
+deltas = (1.0mm, 1.1mm, 0.9mm)
 dims = (2^8, 2^8+2, 2^6)
 offsets = (0.5, 0.5, 0.5) # for FFT spectra later
-#x = (-M÷2:M÷2-1) * dx
-#y = (-N÷2:N÷2-1) * dy
-#jim(x, y, img)
-
 ig = ImageGeom( ; dims, deltas, offsets)
 img = phantom(axes(ig)..., [ob])
-p1 = jim(axes(ig), img, "Ellipsoid phantom", xlabel="x", ylabel="y")
+p1 = jim(axes(ig), img;
+   title="Ellipsoid, rotation ϕ=$ϕ0s", xlabel="x", ylabel="y")
+
+
+# The image integral should match the ellipsoid volume:
+volume = 4/3*π*prod(ob.width)
+(sum(img)*prod(ig.deltas), volume)
 
 
 # Show middle slices
-
 jim(mid3(img), "Middle 3 planes")
+
 
 #=
 ### Spectrum using `spectrum`
@@ -96,8 +99,8 @@ Because the shape has units `mm`, the spectra axes have units cycles/mm.
 Appropriate frequency axes for DFT are provided by `axesf(ig)`.
 =#
 
-zscale = 1 / 4π / prod(radii) # normalize spectra by volume
-spectrum_exact = spectrum(axesf(ig)..., [ob]) * zscale
+vscale = 1 / volume # normalize spectra by volume
+spectrum_exact = spectrum(axesf(ig)..., [ob]) * vscale
 sp = z -> max(log10(abs(z)/oneunit(abs(z))), -6) # log-scale for display
 clim = (-6, 0) # colorbar limit for display
 (xlabel, ylabel) = ("ν₁", "ν₂")
@@ -110,14 +113,15 @@ function myfft(x)
     return fftshift(fft(fftshift(x) / u)) * u
 end
 
-spectrum_fft = myfft(img) * prod(ig.deltas) * zscale
+spectrum_fft = myfft(img) * prod(ig.deltas) * vscale
 p3 = jim(axesf(ig), sp.(spectrum_fft), "log10|DFT|"; clim, xlabel, ylabel)
 
 
 # Compare the DFT and analytical spectra to validate the code
 @assert maximum(abs, spectrum_exact - spectrum_fft) /
-        maximum(abs, spectrum_exact) < 3e-3
-p4 = jim(axesf(ig), abs.(spectrum_fft - spectrum_exact), "Difference"; xlabel, ylabel)
+        maximum(abs, spectrum_exact) < 2e-3
+p4 = jim(axesf(ig), 10^4*abs.(spectrum_fft - spectrum_exact);
+   title="Difference × 10⁴", xlabel, ylabel)
 jim(p1, p4, p2, p3)
 
 
@@ -128,30 +132,37 @@ Compute 2D projection views of the object using `radon`.
 Validate it using the projection-slice theorem aka Fourier-slice theorem.
 =#
 
-end # todo
-
-du, dv = 0.3mm, 0.5mm # projection view pixel spacing
-nu, nv = 2^9, 2^8 # projection view dimensions
-u = (-nu÷2:nu÷2-1) * du # projection view samples
-v = (-nv÷2:nv÷2-1) * dv
-fu = (-nu÷2:nu÷2-1) / nu / du # corresponding spectral axes
-fv = (-nv÷2:nv÷2-1) / nv / dv
-ϕs, θs = :(π/2), :(π/7)
-ϕ, θ = eval.((ϕs, θs))
-proj = radon(u, v, [ϕ], [θ], [ob]) # single projection of a single object
-#p5 = jim(u, v, proj, "Projection at ϕ=$ϕs θ=$θs", xlabel="u", ylabel="v")
+pg = ImageGeom((2^9,2^8), (0.3mm,0.5mm), (0.5,0.5)) # projection sampling
+ϕs, θs = (:(π/2), ϕ0s), (:(π/7), :(0))
+ϕ, θ = [eval.(ϕs)...], [eval.(θs)...]
+proj2 = [radon(axes(pg)..., [ϕ[i]], [θ[i]], [ob]) for i in 1:2] # 2 projections
+p5 = jim(axes(pg)..., proj2; xlabel="u", ylabel="v", title =
+    "Projections at (ϕ,θ) = ($(ϕs[1]), $(θs[1])) and ($(ϕs[2]), $(θs[2]))")
 
 
+#=
+Because the ellipsoid has major axis of length 70mm
+and one of the two views above was along that axis,
+the maximum projection value is about 70mm.
+
+The integral of each projection should match the ellipsoid volume:
+=#
+((p -> sum(p)*prod(pg.deltas)).(proj2)..., 4/3*π*prod(ob.width))
+
+
+# Look at a set of projections as the views orbit around the object.
 ϕd = 0:6:360
 ϕs = deg2rad.(ϕd) # * Unitful.rad # todo round unitful Unitful.°
-proj = radon(u, v, ϕs, [θ], [ob]) # many projection views
-#src p5 = jim(u, v, proj; title="projection views")
+θs = :(π/7)
+θ = eval(θs)
+projs = radon(axes(pg)..., ϕs, [θ], [ob]) # many projection views
+#src p? = jim(axes(pg), projs; title="projection views")
 
-clim = 2 * maximum(radii) * ob.value; clim = (zero(clim), clim)
-for (ip, ϕ) in enumerate(ϕd)
-    jim(u, v, proj[:,:,ip,1]; title="ϕ=$ϕ", clim, prompt=false)
-    gui() # todo anim
+anim = @animate for ip in 1:length(ϕd)
+    jim(axes(pg), projs[:,:,ip,1]; xlabel="u", ylabel="v", prompt=false,
+        title="ϕ=$(ϕd[ip])° θ=$θs", clim = (0,1) .* (2 * maximum(radii) * ob.value))
 end
+gif(anim, "ellipsoid.gif", fps = 6)
 
 
 #=
@@ -161,82 +172,45 @@ by sampling `(u, v, ϕ, θ)` appropriately.
 See Sinograms.jl.
 =#
 
-todo
 
+#=
+### Fourier-slice theorem illustration
 
-# ### Fourier-slice theorem illustration
+Pick one particular view and compare its FFT
+to a slice through the 3D object spectrum.
+=#
 
-# Pick one particular view angle (55°) and look at its slice and spectra.
-ia = argmin(abs.(ϕ .- 55°))
-slice = sino[:,ia]
-slice_fft = myfft(slice) * dr
-angle = round(rad2deg(ϕ[ia]), digits=1)
+ϕs, θs = :(π/3), :(π/7)
+ϕ, θ = eval.((ϕs, θs))
+proj = radon(axes(pg)..., [ϕ], [θ], [ob])[:,:,1,1]
+p6 = jim(axes(pg), proj; xlabel="u", ylabel="v", prompt=false,
+    title = "Projection at (ϕ,θ) = ($ϕs, $θs)")
 
-kx, ky = (fr * cos(ϕ[ia]), fr * sin(ϕ[ia])) # Fourier-slice theorem
-slice_ft = spectrum(ob).(kx, ky)
-@assert maximum(abs, slice_ft - slice_fft) / maximum(abs, slice_ft) < 2e-4
+e1 = (cos(ϕ), sin(ϕ), 0)
+e3 = (sin(ϕ)*sin(θ), -cos(ϕ)*sin(θ), cos(θ))
+fu, fv = ndgrid(axesf(pg)...)
+ff = vec(fu) * [e1...]' + vec(fv) * [e3...]' # fx,fy,fz for Fourier-slice theorem
+spectrum_slice = spectrum(ob).(ff[:,1], ff[:,2], ff[:,3]) * vscale
+spectrum_slice = reshape(spectrum_slice, pg.dims)
+clim = (-6, 0) # colorbar limit for display
+(xlabel, ylabel) = ("νᵤ", "νᵥ")
+p7 = jim(axesf(pg), sp.(spectrum_slice); prompt=false,
+    title = "log10|Spectrum Slice|", clim, xlabel, ylabel)
+proj_fft = myfft(proj) * prod(pg.deltas) * vscale
+p8 = jim(axesf(pg), sp.(proj_fft); prompt=false,
+     title = "log10|FFT Spectrum|", clim, xlabel, ylabel)
 
-p3 = plot(r, slice, title="profile at ϕ = $angle", label="")
-p4 = plot(title="1D spectra")
-scatter!(fr, abs.(slice_fft), label="abs fft", color=:blue)
-scatter!(fr, real(slice_fft), label="real fft", color=:green)
-scatter!(fr, imag(slice_fft), label="imag fft", color=:red, xlims=(-1,1).*(1.0/mm))
-
-plot!(fr, abs.(slice_ft), label="abs", color=:blue)
-plot!(fr, real(slice_ft), label="real", color=:green)
-plot!(fr, imag(slice_ft), label="imag", color=:red)
-plot(p1, p5, p3, p4)
+@assert maximum(abs, spectrum_slice - proj_fft) /
+        maximum(abs, spectrum_slice) < 3e-4
+p9 = jim(axesf(pg), 10^4*abs.(proj_fft - spectrum_slice);
+    title="Difference × 10⁴", xlabel, ylabel, prompt=false)
+jim(p6, p7, p8, p9)
 
 
 #=
-The good agreement between the analytical spectra (solid lines)
-and the DFT samples (disks)
+The good agreement between
+the 2D slice through the 3D analytical spectrum
+and the FFT of the 2D projection view
 validates that `phantom`, `radon`, and `spectrum`
-are all self consistent for this `Ellipsoid` object.
-=#
-
-
-
-#=
-todo: pieces below here
-=#
-
-    # test sinogram with projection-slice theorem
-
-    du,dv = 0.02m, 0.03m
-    nu,nv = 2^9, 2^8
-    u = (-nu÷2:nu÷2-1) * du
-    v = (-nv÷2:nv÷2-1) * dv
-    fu = (-nu÷2:nu÷2-1) / nu / du
-    fv = (-nv÷2:nv÷2-1) / nv / dv
-    ϕ = deg2rad.(0:6:180) # * Unitful.rad # todo round unitful Unitful.°
-    θ = [π/7]
-    sino = @inferred radon(u, v, ϕ, θ, [ob])
-
-#=
-todo
-    ia = argmin(abs.(ϕ .- deg2rad(55)))
-    slice = sino[:,ia]
-    Slice = myfft(slice) * dr
-    angle = round(rad2deg(ϕ[ia]), digits=1)
-
-    kx, ky = (fr * cos(ϕ[ia]), fr * sin(ϕ[ia])) # Fourier-slice theorem
-    ideal = spectrum(ob).(kx, ky)
-
-if DEBUG
-    p2 = jim(r, rad2deg.(ϕ), sino; aspect_ratio=:none, title="sinogram")
-    jim(p1, p2)
-    p3 = plot(r, slice, title="profile at ϕ = $angle", label="")
-    p4 = scatter(fr, abs.(Slice), label="abs fft", color=:blue)
-    scatter!(fr, real(Slice), label="real fft", color=:green)
-    scatter!(fr, imag(Slice), label="imag fft", color=:red,
-        xlims=(-1,1).*(1.2/m), title="1D spectra")
-
-    plot!(fr, abs.(ideal), label="abs", color=:blue)
-    plot!(fr, real(ideal), label="real", color=:green)
-    plot!(fr, imag(ideal), label="imag", color=:red)
-    plot(p1, p2, p3, p4); gui()
-end
-
-    @test maximum(abs, ideal - Slice) / maximum(abs, ideal) < 2e-4
+are all self consistent for this object.
 =#
