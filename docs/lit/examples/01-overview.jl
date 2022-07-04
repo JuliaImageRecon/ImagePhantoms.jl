4edaa3b36f3e41d0974c255716e02480d59a12e5
Jeff Fessler <JeffFessler@users.noreply.github.com>
2022-07-04 10:02:32 -0400

    Document 2D & 3D projection geometry (#27)
    * doc 3d geom
    
    * finish 3d geom
    
    * 2d docs

--- docs/lit/examples/01-overview.jl
+++ docs/lit/examples/01-overview.jl
@@ -62,7 +62,9 @@ we need not only the phantom image, but also its
 and spectrum.
 
 The 2D sinogram coordinate system used here is
-``p(r, ϕ) = \int_{-∞}^{∞} f(r \cos ϕ - ℓ \sin ϕ, r \sin ϕ + ℓ \cos ϕ) \, \mathrm{d} ℓ``.
+```math
+p(r, ϕ) = \int_{-∞}^{∞} f(r \cos ϕ - ℓ \sin ϕ, r \sin ϕ + ℓ \cos ϕ) \, \mathrm{d} ℓ.
+```
 
 We start with the vector of ellipses that defines the phantom,
 using a typical field of view (FOV) of 200mm for a head:
@@ -80,7 +82,7 @@ to use `ImageGeoms` to help with the sampling.
 
 ig = ImageGeom(dims=(200,256), deltas=(1mm,1mm))
 image = phantom(axes(ig)..., objects)
-jim(axes(ig)..., image, xlabel="x", ylabel="y", title="SheppLoganToft")
+jim(axes(ig), image, xlabel="x", ylabel="y", title="SheppLoganToft")
 
 
 # Here is the sinogram corresponding to this phantom,
@@ -89,14 +91,14 @@ jim(axes(ig)..., image, xlabel="x", ylabel="y", title="SheppLoganToft")
 r = LinRange(-100mm,100mm,401)
 ϕ = deg2rad.(0:180)
 sino = radon(r, ϕ, objects)
-jim(r, ϕ, sino, title="Sinogram", yflip=false, aspect_ratio=:none)
+jim(r, ϕ, sino, title="Sinogram")
 
 
 # Here is the 2D spectrum (Fourier transform) of this phantom,
 # computed analytically from the ellipse parameters using `spectrum`:
 
 kspace = spectrum(axesf(ig)..., objects)
-jim(axesf(ig)..., log10.(abs.(kspace/(1mm)^2)), xlabel="ν₁", ylabel="ν₂", title="log10|Spectrum|")
+jim(axesf(ig), log10.(abs.(kspace/(1mm)^2)), xlabel="ν₁", ylabel="ν₂", title="log10|Spectrum|")
 
 #=
 The 2D Fourier transform formula used here is:
---
+++ docs/lit/examples/30-3d.jl
@@ -0,0 +1,129 @@
+#---------------------------------------------------------
+# # [3D geometry](@id 30-3d)
+#---------------------------------------------------------
+
+#=
+This page explains the 3D X-ray transform geometry
+for the Julia package
+[`ImagePhantoms`](https://github.com/JuliaImageRecon/ImagePhantoms.jl).
+
+This page was generated from a single Julia file:
+[30-3d.jl](@__REPO_ROOT_URL__/30-3d.jl).
+=#
+
+#md # In any such Julia documentation,
+#md # you can access the source code
+#md # using the "Edit on GitHub" link in the top right.
+
+#md # The corresponding notebook can be viewed in
+#md # [nbviewer](http://nbviewer.jupyter.org/) here:
+#md # [`30-3d.ipynb`](@__NBVIEWER_ROOT_URL__/30-3d.ipynb),
+#md # and opened in [binder](https://mybinder.org/) here:
+#md # [`30-3d.ipynb`](@__BINDER_ROOT_URL__/30-3d.ipynb).
+
+
+# ### Setup
+
+# Packages needed here.
+
+using ImagePhantoms
+using ImageGeoms: ImageGeom, axesf
+using MIRTjim: jim, prompt
+using UnitfulRecipes
+using Unitful: mm
+using InteractiveUtils: versioninfo
+
+# The following line is helpful when running this file as a script;
+# this way it will prompt user to hit a key after each figure is displayed.
+
+isinteractive() ? jim(:prompt, true) : prompt(:draw);
+
+
+#=
+### Overview
+
+Most practical tomographic imaging problems are 3D,
+so this package also supports calculating
+3D line integrals through 3D phantom objects.
+
+### 3D projection geometry
+
+Given a 3D phantom object,
+the function `radon`
+returns a 4-argument function
+having arguments
+``(u,v,ϕ,θ)``,
+where ``u,v`` denote the coordinates
+on a 2D projection plane,
+``ϕ`` denotes the
+[azimuthal angle](https://en.wikipedia.org/wiki/Azimuth)
+angle
+and
+``θ`` denotes the
+[polar angle](https://en.wikipedia.org/wiki/Spherical_coordinate_system).
+
+The mathematical definition of the
+[Radon transform of a 3D function](https://en.wikipedia.org/wiki/Radon_transform)
+is a collection of (2D) plane integrals,
+whereas the
+[X-ray transform](https://en.wikipedia.org/wiki/X-ray_transform)
+is a collection of (1D) line integrals.
+See 
+[Section II.1, Natterer 2001](http://doi.org/10.1137/1.9780898719284).
+So strictly speaking the `radon` function is a misnomer in 3D,
+whereas for 2D functions
+the Radon transform and the X-ray transform coincide.
+
+The coordinate system used here is defined as follows.
+Start by defining a point on the "detector plane" as follows:
+```math
+\vec{p} = \vec{p}(u, v, ϕ, θ)
+= u \vec{e}_1 + v \vec{e}_3
+= (u \cos ϕ + v \sin ϕ \sin θ, u \sin ϕ - v \cos ϕ \sin θ, v \cos θ) ∈ ℝ^3,
+```
+where
+```math
+\vec{e}_1 = (\cos ϕ, \sin ϕ, 0)
+,\qquad
+\vec{e}_3 = (\sin ϕ \sin θ, -\cos ϕ \sin θ, \cos θ).
+```
+
+Now define the (X-ray) projection
+of a 3D object ``f(\vec{x})``
+as
+```math
+p(u, v, ϕ, θ)
+= ∫ f(\vec{p} + ℓ \, \vec{e}) \, \mathrm{d} ℓ
+= ∫_{-∞}^{∞} f( \vec{p}(u,v,ϕ,θ) + ℓ \, \vec{e}(ϕ,θ) ) \, \mathrm{d} ℓ,
+```
+where
+```math
+\vec{e}(ϕ,θ) = (-\sin ϕ \cos θ, \cos ϕ \cos θ, \sin θ).
+```
+
+When ``θ=0``,
+then the X-ray transform
+``p(u, v, ϕ, θ)``
+is a collection of 2D sinograms,
+one for each slice of ``f(x,y,z)``:
+```math
+p(u, v, ϕ, 0)
+= ∫_{-∞}^{∞} f(u \cos ϕ - ℓ \sin ϕ, u \sin ϕ + ℓ \cos ϕ, v) \, \mathrm{d} ℓ.
+```
+
+For example projection views,
+see the Ellipsoid examples.
+
+=#
+
+
+# ## Reproducibility
+
+# This page was generated with the following version of Julia:
+
+io = IOBuffer(); versioninfo(io); split(String(take!(io)), '\n')
+
+
+# And with the following package versions
+
+import Pkg; Pkg.status()
