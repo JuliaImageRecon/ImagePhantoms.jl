ca19f1389c2805beb55d66c47b8c1a85a7b7aaba
Jeff Fessler <JeffFessler@users.noreply.github.com>
2022-07-14 16:52:39 -0400

    towards matching 2d/3d

--- src/object2.jl
+++ src/object2.jl
@@ -6,69 +6,15 @@ Utilities for 2D objects
 #export rotate, scale, translate # no: to avoid conflicts with Plots
 export phantom, radon, spectrum
 
-_tuple(x::Any, n::Int) = ntuple(i -> x, n)
-
-
-
-end
-
 
 # rotate
+
 """
     rotate(ob::Object2d, θ::RealU)
-In-plane rotation of a 2D object.
+Rotate a 2D object.
 """
 rotate(ob::Object2d, θ::RealU) =
     Object(ob.shape, ob.center, ob.width, ob.angle .+ (θ,), ob.value, ob.param)
-#rotate(ob::Object3d, θ::RealU) =
-#Object(ob.shape, ob.center, ob.width, ob.angle .+ (θ,0), ob.value, ob.param)
-
-
-"""
-    rotate(ob::Object3d, (α,β))
-    rotate(ob::Object3d, α, β=0)
-Rotation of a 3D object.
-"""
-rotate(ob::Object3d, θ::NTuple{2,RealU}) =
-    Object(ob.shape, ob.center, ob.width, ob.angle .+ θ, ob.value, ob.param)
-rotate(ob::Object3d, α::RealU, β::RealU=0) = rotate(ob, (α,β))
-
-
-# scale width (tricky because length(width) < D is possible
-"""
-    scale(ob::Object, factor::RealU)
-    scale(ob::Object, factor::NTuple{W,RealU})
-Scale the width(s) by `factor`.
-"""
-scale(ob::Object{S,D,V,W}, factor::NTuple{W,RealU}) where {S,D,V,W} =
-    Object(ob.shape, ob.center, ob.width .* factor, ob.angle, ob.value, ob.param)
-scale(ob::Object{S,D,V,W}, factor::RealU) where {S,D,V,W} =
-    scale(ob, _tuple(factor,W))
-
-
-# scale value
-"""
-    (*)(ob::Object, x::Number)
-    (*)(x::Number, ob::Object)
-Scale object `value` by `x`.
-"""
-Base.:(*)(ob::AbstractObject, x::Number) =
-    Object(ob.shape, ob.center, ob.width, ob.angle, ob.value * x, ob.param)
-Base.:(/)(ob::Object, x::Number) = ob * (1 / x)
-Base.:(*)(x::Number, ob::Object) = ob * x
-
-
-# translate
-"""
-    translate(ob::Object, shift::NTuple{D,RealU})
-    translate(ob::Object2d, xshift, yshift)
-    translate(ob::Object3d, xshift, yshift, zshift)
-Translate the center coordinates of an object by `shift`
-"""
-translate(ob::Object{S,D}, shift::NTuple{D,RealU}) where {S, D} =
-    Object(ob.shape, ob.center .+ shift, ob.width, ob.angle, ob.value, ob.param)
-translate(ob::Object3d, x::RealU, y::RealU, z::RealU) = translate(ob, (x,y,z))
-translate(ob::Object2d, x::RealU, y::RealU) = translate(ob, (x,y))
 
 
 """
@@ -76,36 +22,57 @@ translate(ob::Object2d, x::RealU, y::RealU) = translate(ob, (x,y))
 Put coordinates `(x,y)` in canonical axes associated with `object`.
 """
 function coords(ob::Object2d, x::RealU, y::RealU)
-    (x, y) = rotate2d(x - ob.center[1], y - ob.center[2], ob.angle[1])
-    return (x / ob.width[1], y / ob.width[2]) # unitless
+    xy = rotate2d(x - ob.center[1], y - ob.center[2], ob.angle[1])
+    return xy ./ ob.width # unitless
+end
+
+
+# phantom images
+
+
+"""
+    phantom(ob::Object2d)::Function
+Return function of `(x,y)`
+that user can sample at any locations
+to make a 2D phantom image
+for a single 2D object.
+"""
+function phantom(ob::Object2d)
+    # apply coordinate transformation and value:
+    return (x,y) -> ob.value * phantom1(ob, coords(ob, x, y))
 end
 
 
 """
-    coords(object::Object3d, x::RealU, y::RealU, y::RealU)
-Put coordinates `(x,y,z)` in canonical axes associated with `object`.
-"""
-function coords(ob::Object3d, x::RealU, y::RealU, z::RealU)
-    (x, y, z) = rotate3d(x - ob.center[1], y - ob.center[2], z - ob.center[3],
-        ob.angle[1], ob.angle[2])
-    return (x / ob.width[1], y / ob.width[2], z / ob.width[3]) # unitless
-end
-
-
-# methods for phantoms: an array of objects
-
-"""
-    image = phantom(oa::Array{<:Object2d})::Function
-Return function `image(x,y)` that user can sample at any `(x,y)` locations
-to make a phantom image.
+    phantom(oa::Array{<:Object2d})::Function
+Return function of `(x,y)`
+that user can sample at any locations
+to make a 2D phantom image
+for an `Array` of 2D objects.
 """
 function phantom(oa::Array{<:Object2d})
     return (x,y) -> sum(ob -> phantom(ob)(x,y), oa)
 end
 
+
 """
-    image = phantom(x, y, oa::Array{<:Object2d}, oversample::Int; T)
-Return a digital image of the phantom sampled at `(x,y)` locations,
+    phantom(x::Vector, y::Vector, oa::Array{<:Object2d})
+Return a 2D digital image of the phantom sampled at grid of `(x,y)` locations.
+Returned 2D image size is `length(x) × length(y)`.
+"""
+function phantom(
+    x::AbstractVector,
+    y::AbstractVector,
+    oa::Array{<:Object2d},
+)
+    return phantom(oa).(x, y')
+end
+
+
+"""
+    phantom(x::Vector, y::Vector, oa::Array{<:Object2d}, oversample::Int; T)
+Return a 2D digital image of the phantom sampled
+at grid of `(x,y)` locations,
 with over-sampling factor `oversample` and element type `T`.
 """
 function phantom(
@@ -115,47 +82,96 @@ function phantom(
     oversample::Int;
     T::DataType = promote_type(eltype.(oa)..., Float32),
 )
+
     oversample < 1 && throw(ArgumentError("oversample $oversample"))
     dx = x[2] - x[1]
     dy = y[2] - y[1]
     all(≈(dx), diff(x)) || throw("oversample requires uniform x")
     all(≈(dy), diff(y)) || throw("oversample requires uniform y")
     tmp = ((1:oversample) .- (oversample+1)/2) / oversample
-    ophantom = ob ->
-       (x,y) -> T(sum(phantom(ob).(x .+ dx*tmp, y .+ dy*tmp')) / abs2(oversample))
+    ophantom = ob -> (x,y) ->
+        T(sum(phantom(ob).(x .+ dx*tmp, y .+ dy*tmp')) / abs2(oversample))
     return sum(ob -> ophantom(ob).(x,y'), oa)
 end
 
-"""
-    image = phantom(x, y, oa::Array{<:Object2d})
-Return a digital image of the phantom sampled at `(x,y)` locations.
-"""
-function phantom(
-    x::AbstractVector,
-    y::AbstractVector,
-    oa::Array{<:Object2d},
-)
 
-    return sum(ob -> phantom(ob).(x,y'), oa)
+# radon transform
+
+# shift property of the X-ray transform
+function xray_shift(
+    r::RealU, ϕ::RealU, # projection coordinates
+    cx::RealU, cy::RealU, # object center
+)
+    (sϕ, cϕ) = sincos(ϕ)
+    rshift = cx * cϕ + cy * sϕ
+    return r - rshift
 end
 
-function phantom(
-    xx::AbstractArray,
-    yy::AbstractArray,
-    oa::Array{<:Object2d},
+# rotation property
+function xray_rotate(
+    Δr::RealU, ϕ::RealU, # projection coordinates
+    Φazim::RealU,
 )
-    return sum(ob -> phantom(ob).(xx,yy), oa)
+    return (Δr, ϕ - Φazim)
+end
+
+# affine scaling property of the X-ray transform
+function xray_scale(
+    r::RealU, ϕ::RealU, # projection coordinates
+    wx::RealU, wy::RealU, # object width
+)
+    (sϕ, cϕ) = sincos(ϕ)
+    e1old = [cϕ, sϕ]
+    e2old = [-sϕ, cϕ]
+    width = [wx, wy]
+    r = 1 / sqrt( sum(abs2, e2old ./ width) ) # would be radius for circle
+    ϕp = atan(wy * sϕ, wx * cϕ) # ϕ'
+    tmp = (e1old * r) ./ width
+    (sϕp, cϕp) = sincos(ϕp)
+    e1new = [cϕp, sϕp]
+    up = e1new' * tmp
+    return (r, up, ϕp)
+end
+
+
+# interface to xray1 after applying shift, rotate, scale properties
+function _xray(
+    type::AbstractShape2,
+    center::Tuple,
+    width::Tuple,
+    angle::RealU,
+    r::RealU, ϕ::RealU,
+)
+    Δr, ϕ = xray_shift(r, ϕ, center...)
+    rr, ϕr = xray_rotate(Δr, ϕ, angle)
+    scale, rp, ϕp, = xray_scale(rr, ϕr, width...)
+    return scale * xray1(type, rp, ϕp)
 end
 
 
 """
-    sino = radon(oa::Array{<:Object2d})::Function
-Return function `sino(r,ϕ)` that user can sample at any `(r,ϕ)` locations
-to make a phantom 2D sinogram.
+    radon(ob::Object2d)::Function
+Return function of `(r,ϕ)` that user can sample
+at any projection coordinates
+to make sinogram views of a 2D object.
 
 The coordinate system used here is such that `ϕ=0` corresponds to
-line integrals along the ``y`` axis for an object ``f(x,y)``.
+line integrals along the ``y`` axis
+for an object ``f(x,y)``.
 Then as `ϕ` increases, the line integrals rotate counter-clockwise.
+"""
+function radon(ob::Object2d)
+    return (r,ϕ) -> ob.value *
+        _xray(ob.shape, ob.center, ob.width, ob.angle, r, ϕ)
+end
+
+
+"""
+    radon(oa::Array{<:Object2d})::Function
+Return function of `(r,ϕ)` that user can sample
+at any sinogram coordinates
+to make a phantom 2D sinogram
+for an array of 3D objects.
 """
 function radon(oa::Array{<:Object2d})
     return (r,ϕ) -> sum(ob -> radon(ob)(r,ϕ), oa)
@@ -163,8 +179,10 @@ end
 
 
 """
-    sino = radon(r, ϕ, oa::Array{<:Object2d})
-Return parallel-beam 2D sinogram `sino` sampled at given `(r,ϕ)` locations.
+    radon(r::Vector, ϕ::Vector, oa::Array{<:Object2d})
+Return parallel-beam 2D sinogram
+sampled at grid of `(r,ϕ)` locations.
+Returned array size is `length(r) × length(ϕ)`.
 """
 function radon(
     r::AbstractVector,
@@ -172,6 +190,21 @@ function radon(
     oa::Array{<:Object2d},
 )
     return sum(ob -> radon(ob).(r,ϕ'), oa)
+end
+
+
+"""
+    radon(r::Vector, ϕ::RealU, oa::Array{<:Object2d})
+Return parallel-beam projection
+sampled at grid of `r` locations for one `ϕ` value.
+Returned array size is `length(r)`.
+"""
+function radon(
+    r::AbstractVector,
+    ϕ::RealU,
+    oa::Array{<:Object2d},
+)
+    return radon(oa).(r, ϕ)
 end
 
 
