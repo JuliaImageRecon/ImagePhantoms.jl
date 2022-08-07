bced27e0ee7a7df6034afcf1edb25ef5e9dab42b
Jeff Fessler <fessler@umich.edu>
2022-08-07 10:38:16 -0400

    rm DEBUG code

--- test/ellipse.jl
+++ test/ellipse.jl
@@ -2,21 +2,12 @@
 ellipse.jl
 =#
 
-const DEBUG = false
-
 using ImagePhantoms: Object, Object2d, AbstractShape, phantom, radon, spectrum
 using ImagePhantoms: Ellipse, ellipse, circle
 import ImagePhantoms as IP
 using Unitful: m, unit, °
 using FFTW: fftshift, fft
 using Test: @test, @testset, @test_throws, @inferred
-if DEBUG
-    include("helper.jl")
-    using MIRTjim: jim, prompt
-    using UnitfulRecipes
-    using Plots: plot, plot!, scatter, scatter!, gui, default
-    default(markerstrokecolor=:auto, markersize=2)
-end
 
 (Shape, shape, shape2) = (Ellipse, ellipse, circle)
 
@@ -127,16 +118,6 @@ end
     X = myfft(img) * dx * dy * zscale
     kspace = @inferred spectrum(fx, fy, [ob]) * zscale
 
-if DEBUG
-    clim = (-6, 0)
-    sp = z -> max(log10(abs(z)/oneunit(abs(z))), -6)
-    p1 = jim(x, y, img, "phantom")
-    p2 = jim(fx, fy, sp.(X), "log10|DFT|"; clim)
-    p3 = jim(fx, fy, sp.(kspace), "log10|Spectrum|"; clim)
-    p4 = jim(fx, fy, abs.(kspace - X), "Difference")
-    jim(p1, p4, p2, p3); prompt()
-end
-
     @test abs(maximum(abs, X) - 1) < 1e-6
     @test abs(maximum(abs, kspace) - 1) < 1e-5
     @test maximum(abs, kspace - X) / maximum(abs, kspace) < 6e-4
@@ -158,21 +139,6 @@ end
 
     kx, ky = (fr * cos(ϕ[ia]), fr * sin(ϕ[ia])) # Fourier-slice theorem
     ideal = spectrum(ob).(kx, ky)
-
-if DEBUG
-    p2 = jim(r, rad2deg.(ϕ), sino; aspect_ratio=:none, title="sinogram")
-    jim(p1, p2)
-    p3 = plot(r, slice, title="profile at ϕ = $angle", label="")
-    p4 = scatter(fr, abs.(Slice), label="abs fft", color=:blue)
-    scatter!(fr, real(Slice), label="real fft", color=:green)
-    scatter!(fr, imag(Slice), label="imag fft", color=:red,
-        xlims=(-1,1).*(1.2/m), title="1D spectra")
-
-    plot!(fr, abs.(ideal), label="abs", color=:blue)
-    plot!(fr, real(ideal), label="real", color=:green)
-    plot!(fr, imag(ideal), label="imag", color=:red)
-    plot(p1, p2, p3, p4); gui()
-end
 
     @test maximum(abs, ideal - Slice) / maximum(abs, ideal) < 2e-4
 end
--- test/gauss2.jl
+++ test/gauss2.jl
@@ -2,21 +2,12 @@
 gauss2.jl
 =#
 
-const DEBUG = false
-
 using ImagePhantoms: Object, Object2d, AbstractShape, phantom, radon, spectrum
 using ImagePhantoms: Gauss2, gauss2
 import ImagePhantoms as IP
 using Unitful: m, unit, °
 using FFTW: fftshift, fft
 using Test: @test, @testset, @test_throws, @inferred
-if DEBUG
-    include("helper.jl")
-    using MIRTjim: jim, prompt
-    using UnitfulRecipes
-    using Plots: plot, plot!, scatter, scatter!, gui, default
-    default(markerstrokecolor=:auto, markersize=2)
-end
 
 (Shape, shape) = (Gauss2, gauss2)
 
@@ -90,14 +81,6 @@ end
     ob = @inferred shape((0, 0), (fwhm, Inf), 0, 1)
     tmp = @inferred phantom((-1:1)*fwhm/2, [0], [ob])
     @test tmp ≈ [0.5, 1, 0.5]
-
-if DEBUG # check profile
-    x = -2fwhm:2fwhm
-    profile = @inferred phantom(x, [0], [ob])
-    scatter(x, profile, label="profile")
-    scatter!([-1,1]*fwhm/2, [1,1]*0.5, label="fwhm/2")
-#   prompt()
-end
 end
 
 
@@ -116,16 +99,6 @@ end
     fy = (-N÷2:N÷2-1) / N / dy
     X = myfft(img) * dx * dy * zscale
     kspace = @inferred spectrum(fx, fy, [ob]) * zscale
-
-if DEBUG
-    clim = (-6, 0)
-    sp = z -> max(log10(abs(z)/oneunit(abs(z))), -6)
-    p1 = jim(x, y, img, "phantom")
-    p2 = jim(fx, fy, sp.(X), "log10|DFT|"; clim)
-    p3 = jim(fx, fy, sp.(kspace), "log10|Spectrum|"; clim)
-    p4 = jim(fx, fy, 1e6*abs.(kspace - X), "Difference * 1e6")
-    jim(p1, p4, p2, p3); prompt()
-end
 
     @test abs(maximum(abs, X) - 1) < 1e-6
     @test abs(maximum(abs, kspace) - 1) < 1e-6
@@ -148,22 +121,6 @@ end
 
     kx, ky = (fr * cos(ϕ[ia]), fr * sin(ϕ[ia])) # Fourier-slice theorem
     ideal = spectrum(ob).(kx, ky)
-
-if DEBUG
-    @show maximum(abs, ideal - Slice) / maximum(abs, ideal)
-    p2 = jim(r, rad2deg.(ϕ), sino; aspect_ratio=:none, title="sinogram")
-    jim(p1, p2)
-    p3 = plot(r, slice, title="profile at ϕ = $angle", label="")
-    p4 = scatter(fr, abs.(Slice), label="abs fft", color=:blue)
-    scatter!(fr, real(Slice), label="real fft", color=:green)
-    scatter!(fr, imag(Slice), label="imag fft", color=:red,
-        xlims=(-1,1).*(1.2/m), title="1D spectra")
-
-    plot!(fr, abs.(ideal), label="abs", color=:blue)
-    plot!(fr, real(ideal), label="real", color=:green)
-    plot!(fr, imag(ideal), label="imag", color=:red)
-    plot(p1, p2, p3, p4); gui()
-end
 
     @test maximum(abs, ideal - Slice) / maximum(abs, ideal) < 4e-4
 end
