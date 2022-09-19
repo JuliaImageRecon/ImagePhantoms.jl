49e8cd716fc8120f398a423677b61b486d40b194
Jeff Fessler <fessler@umich.edu>
2022-09-19 10:11:27 +0200

    refine dirac2

--- src/dirac2.jl
+++ src/dirac2.jl
@@ -1,12 +1,9 @@
 #=
-dirac.jl
+dirac2.jl
 =#
 
-
-using ImagePhantoms: Object, AbstractShape
-
-export Dirac, dirac
-export spectrum #phantom, radon
+export Dirac2, dirac2
+#export Dirac
 
 
 """
@@ -14,49 +11,52 @@ export spectrum #phantom, radon
 """
 struct Dirac{D} <: AbstractShape{D} end
 
+const Dirac2 = Dirac{2}
+
 
 # constructor
 
 
 """
-    dirac2(cx, cy, wx=1, wy=wx, ϕ=0, value::Number=1)
-    dirac2(center::NTuple{2,RealU}, width::NTuple{2,RealU}=(1,1), ϕ::RealU=0, v=1)
-Construct `Object{Dirac}` from parameters;
-here `width` is the full-width.
+    dirac2(...)
+Construct `Object2d{Dirac2}` from parameters;
+here `width` is a scale factor: `δ((x - c)/w) = w * δ(x - c)`.
 """
-#dirac2(args... ; kwargs...) = Object(Dirac{2}(), args...; kwargs...)
-#dirac3(args... ; kwargs...) = Object(Dirac{3}(), args...; kwargs...)
-dirac(D::Int, args... ; kwargs...) = Object(Dirac{D}(), args...; kwargs...)
+dirac2(args... ; kwargs...) = Object(Dirac2(), args...; kwargs...)
+# dirac(D::Int, args... ; kwargs...) = Object(Dirac{D}(), args...; kwargs...)
 
-
-
-# helper
 
 
 # methods
 
 
-area1(::Dirac) = 1
+area1(::Dirac2) = 1
 
 ℓmax1(::Dirac) = 1 # max line integral
 
 
 """
-    phantom1(ob::Object{Dirac}, (x,y))
+    phantom1(ob::Object2d{Dirac2}, (x,y))
 Evaluate Dirac at `(x,y)`,
 for unitless coordinates.
+Caution: this function is meaningless!
 """
-# phantom1(ob::Object{Dirac, D, V}, xy::NTuple{D,Real}) = (xy == (0,0)) ? Inf : zero(V)
-
-
-# x-ray transform (line integral) of Dirac
-# `r` should be unitless
-# xray1(::Dirac, r::Real, ϕ::RealU) = (r == 0) ? Inf : zero(r)
+phantom1(ob::Object2d{Dirac2}, xy::NTuple{2,Real}) = (xy == (0,0)) ? 1 : 0
 
 
 """
-    spectrum1(::Object{Dirac}, (kx,ky))
-Spectrum of Dirac at `(kx,ky)`,
+    xray1(::Dirac2, r::Real, ϕ::RealU)
+X-ray transform (line integral) of Dirac.
+`r` should be unitless
+Caution: this function is meaningless!
+"""
+xray1(::Dirac2, r::Real, ϕ::RealU) = (r == 0) ? 1 : 0
+
+
+"""
+    spectrum1(::Object2d{Dirac2}, (kx,ky))
+Spectrum of Dirac2 at `(kx,ky)`,
 for unitless spatial frequency coordinates.
 """
-spectrum1(::Object{Dirac, D, V}, kxy::NTuple{D,Real}) where {D, V} = one(V)
+spectrum1(::Object2d{Dirac2}, kxy::NTuple{2,Real}) = 1
+#spectrum1(::Object{Dirac, D, V}, kxy::NTuple{D,Real}) where {D, V} = one(V)
