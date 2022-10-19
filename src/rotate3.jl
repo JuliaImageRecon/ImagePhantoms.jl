a1d36f8b232b8979eff5c14b374c895a5be5351e
Jeff Fessler <JeffFessler@users.noreply.github.com>
2022-10-19 13:29:19 -0400

    clean rotate3

--- src/rotate3.jl
+++ src/rotate3.jl
@@ -3,8 +3,6 @@ rotate3.jl
 3d rotation utilities
 =#
 
-const RealU = Real
-
 
 """
     Rxyz_mul(x::RealU, y::RealU, z::RealU, ϕ::RealU, θ::RealU, ψ::RealU)
@@ -99,50 +97,3 @@ end
 rotate3d(xyz::NTuple{3,RealU}, ϕ::RealU, θ::RealU, ψ::RealU) =
     rotate3d(xyz..., ϕ, θ, ψ)
 =#
-
-
-
-# tests
-# todo - move to test/
-
-Rx(a) = [1 0 0; 0 cos(a) -sin(a); 0 sin(a) cos(a)]
-Ry(a) = [cos(a) 0 sin(a); 0 1 0; -sin(a) 0 cos(a)]
-Rz(a) = [cos(a) -sin(a) 0; sin(a) cos(a) 0; 0 0 1]
-
-using Test
-using LinearAlgebra: I
-
-a = randn()
-@test Rx(a)' ≈ Rx(-a)
-@test Rx(a)' ≈ inv(Rx(a))
-@test Ry(a)' ≈ Ry(-a)
-@test Rz(a)' ≈ inv(Rz(a))
-@test Rz(a)' ≈ Rz(-a)
-@test Ry(a)' ≈ inv(Ry(a))
-
-function Rxyz_mat(ϕ::RealU, θ::RealU, ψ::RealU)
-    return hcat([collect(Rxyz_mul(c..., ϕ, θ, ψ)) for c in eachcol(I(3))]...)
-end
-
-function Rxyz_inv_mat(ϕ::RealU, θ::RealU, ψ::RealU)
-    return hcat([collect(Rxyz_inv(c..., ϕ, θ, ψ)) for c in eachcol(I(3))]...)
-end
-
-a = randn()
-b = randn()
-c = randn()
-
-R = Rxyz_mat(a, b, c)
-@test R' ≈ inv(R)
-
-Ri = Rxyz_inv_mat(a, b, c)
-@test Ri ≈ inv(R)
-
-R = Rxyz_mat(a, 0, 0)
-@test R ≈ Rz(a)
-
-R = Rxyz_mat(0, b, 0)
-@test R ≈ Ry(b)
-
-R = Rxyz_mat(0, 0, c)
-@test R ≈ Rx(c)
